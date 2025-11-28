6039991c0562824ebdab1c1958dfcd3ee58f1aae
daria0sergeevna <daria0sergeevna@gmail.com>
2025-11-28 18:06:57 +0300

    Task №14: Array Deque

---
+++ Task14/Task14/ArrayDequeException.cs
@@ -0,0 +1,20 @@
+﻿namespace Task14;
+
+class ArrayDequeException : Exception
+{
+    public ArrayDequeException() :
+        base("Ошибка!")
+    { }
+}
+class ArrayDequeArgumentNullException : ArgumentNullException
+{
+    public ArrayDequeArgumentNullException() :
+        base("Передаваемый объект не может быть null!")
+    { }
+}
+class ArrayDequeOutOfMemoryException : OutOfMemoryException
+{
+    public ArrayDequeOutOfMemoryException() :
+        base("Слишком большой дек!")
+    { }
+}
---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task14;
+class Program
+{
+    static void Main()
+    {
+        MyArrayDeque<int> myArrayDeque = new MyArrayDeque<int>();
+        for(int i = 0; i < 10; i++)
+            myArrayDeque.Add(i);
+        Console.WriteLine(myArrayDeque.IsEmpty());
+    }
+};
