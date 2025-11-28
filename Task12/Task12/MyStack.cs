670f3cb3bac5c385608858a61a604915e1a5fde6
daria0sergeevna <daria0sergeevna@gmail.com>
2025-11-28 18:04:07 +0300

    Task №12: Stack

---
+++ Task12/Task12/MyStack.cs
@@ -0,0 +1,24 @@
+﻿namespace Task12;
+
+class MyStack<T> : MyVector<T>
+{
+    public void Push(T item) => Add(item);
+    public T Pop()
+    {
+        if (Empty()) return default;
+        T tmp = Peek();
+        Remove(lastIndexOf(tmp));
+        return tmp;
+    }
+    public T Peek()
+    {
+        if (Empty()) throw new VectorArgumentException();
+        return LastElement();
+    }
+    public bool Empty() => IsEmpty();
+    public int Search(T item)
+    {
+        if (!Contains(item)) return -1;
+        return Size() - lastIndexOf(item);
+    }
+}
---
+++ Task12/Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task12;
+class Program
+{
+    static void Main()
+    {
+        MyStack<int> stack = new MyStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(1111114);
+        stack.Push(4);
+        Console.WriteLine(stack.Search(0));
+        Console.WriteLine(stack.Search(2));
+        Console.WriteLine(stack.Search(5));
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+    }
+}
